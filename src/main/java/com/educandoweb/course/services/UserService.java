7a03ecc7605c10b34c36185877be955a45971c44
Rodrigo Felipe <rodrigogfelipe@hotmail.com>
2019-10-02 13:47:21 -0300

    Deleção de usuário com DELETE

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -27,14 +27,19 @@ public class UserService {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-	
-	//Declarando metado para inserir no BD
+
+	// Declarando metado para inserir no BD
 	public User insert(User obj) {
 		return repo.insert(obj);
 	}
 	
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id); 
+	}
+
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
-		
+
 	}
 }
