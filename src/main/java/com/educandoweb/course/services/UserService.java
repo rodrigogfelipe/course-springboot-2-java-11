5ea23b44c9f6fe8e054a6caf510e531d147f665f
Rodrigo Felipe <rodrigogfelipe@hotmail.com>
2019-10-02 14:08:57 -0300

    Atualização de usuário com PUT

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -32,10 +32,23 @@ public class UserService {
 	public User insert(User obj) {
 		return repo.insert(obj);
 	}
-	
+
 	public void delete(String id) {
 		findById(id);
-		repo.deleteById(id); 
+		repo.deleteById(id);
+	}
+
+	// Metado User para atualizar o BD
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+
 	}
 
 	public User fromDTO(UserDTO objDto) {
