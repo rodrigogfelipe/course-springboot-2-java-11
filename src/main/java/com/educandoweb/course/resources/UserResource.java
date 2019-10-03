c9cef17545e4a79ff93e4f5e67827da7d4df0c3c
Rodrigo Felipe <rodrigogfelipe@hotmail.com>
2019-10-03 11:22:56 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.educandoweb.course.domain.Post;
 import com.educandoweb.course.domain.User;
 import com.educandoweb.course.dto.UserDTO;
 import com.educandoweb.course.services.UserService;
@@ -55,11 +56,15 @@ public class UserResource {
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
 	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
-		{
-			User obj = service.fromDTO(objDto);
-			obj.setId(id);
-			obj = service.update(obj);
-			return ResponseEntity.noContent().build();
-		}
+		User obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
